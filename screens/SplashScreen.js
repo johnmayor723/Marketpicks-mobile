a22f353ba63949b703608da5c02b4856b14b8b8f
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-02-14 11:30:14 +0100

    Update SplashScreen.js

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -15,7 +15,7 @@ const SplashScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <Image
-                source={require('../assets/foodlogo.png')} // Make sure you have a logo in the assets folder
+                source={require('../assets/App 1024x1024px.jpg')} // Make sure you have a logo in the assets folder
                 style={styles.logo}
             />
             <Text style={styles.title}>Welcome to FoodDeck-pro</Text>
